e27b083850608b523a2221da160d1592c58cb20f
Anant Mishra <144559155+anntmishra@users.noreply.github.com>
2024-12-20 20:32:41 +0530

    Update executionflow.rs

--- 01-Basic/executionflow.rs
+++ 01-Basic/executionflow.rs
@@ -25,27 +25,16 @@ if a>99{
 
 //nested if else
 
-fn categorize_number(num: i32) -> &'static str {
-    if num > 0 {
-        if num % 2 == 0 {
-            "Positive even number"
-        } else {
-            "Positive odd number"
-        }
-    } else if num < 0 {
-        if num % 2 == 0 {
-            "Negative even number"
-        } else {
-            "Negative odd number"
-        }
-    } else {
-        "Zero"
-    }
-}
 
-fn main() {
-    let number = 5;
+fn test(){
+    let n=99;
+    if n>99{
+        if a>200{
+            prinln!("Huge Number");
+        }else{
+            println!("Big Number");
 
-    // Call the function and print the result
-    println!("The number {} is a {}.", number, categorize_number(number));
-}
+        }
+    }else{
+        prinln!("Small Number");
+    }}
